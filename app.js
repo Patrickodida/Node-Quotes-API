55ef30f774e7d08c1a825be452e1b0bb48bd3b49
Patrickodida <patrick.o.odida@gmail.com>
2024-08-08 13:34:12 +0300

    Add app.js file and set the server to listen on PORT 5000

---
+++ app.js
@@ -0,0 +1,19 @@
+// Import Libraries
+const express = require('express');
+const fs = require('node:fs');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Set request handlers based on the API endpoints
+app.get("/", (req, res)=>{
+    res.send(`<h1 style="color: blue">Welcome to my quote API</h1>`)
+});
+
+// Set the server to listen on port 5000
+const PORT = 5000;
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on http://localhost:${PORT}`)
+});
