31d7f80f9a98614f787ccdae3e387c01d768ef9a
Patrickodida <patrick.o.odida@gmail.com>
2024-08-09 22:02:22 +0300

    Implement middleware for logging request to the 'request_logs.txt' file

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 // Import Libraries
 const express = require('express');
 const fs = require('node:fs');
+const path = require('node:path');
 const authorRouter = require('./Routes/authorsRoutes');
 const quoteRouter = require('./Routes/quotesRoutes');
 
 const app = express();
 
-// Middleware
+// Middleware for Logging Requests
+app.use((req, res, next)=>{
+    // get start time
+    const startTime = new Date();
+    // Move to the next Middleware
+    next();
+    // After response is sent get the end time
+    const endTime = new Date();
+    // Log Details; TImestamp, method, URL, and response status
+    const logDetails = `[${startTime.toISOString()}] ${req.method} ${req.url} ${res.statusCode}\n`;
+    // Append the log details to 'request_logs.txt' file
+    fs.appendFile(path.join(__dirname, 'Logs', 'request_logs.txt'), logDetails, (err)=>{
+        if(err){
+            console.log("Failed to write logs:", err)
+        }
+    })
+
+})
+
+// Middle to parse JSON bodies
 app.use(express.json());
 
 app.use("/authors", authorRouter);
