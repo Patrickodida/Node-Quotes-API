79d5ecfa7f8a7641051fe6747eb647aa622353a5
Patrickodida <patrick.o.odida@gmail.com>
2024-08-13 12:45:33 +0300

    Log duration between endTime and startTime

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
 // Import Libraries
-const express = require('express');
-const fs = require('node:fs');
-const path = require('node:path');
-const authorRouter = require('./Routes/authorsRoutes');
-const quoteRouter = require('./Routes/quotesRoutes');
+const express = require("express");
+const fs = require("node:fs");
+const path = require("node:path");
+const authorRouter = require("./Routes/authorsRoutes");
+const quoteRouter = require("./Routes/quotesRoutes");
 
 const app = express();
 
 // Middleware for Logging Requests
-app.use((req, res, next)=>{
-    // get start time
-    const startTime = new Date();
-    // Move to the next Middleware
-    next();
+app.use((req, res, next) => {
+  // get start time
+  const startTime = new Date();
+  // Listen to the 'finish' listener to capture 'endTime'
+  res.on("finish", () => {
     // After response is sent get the end time
     const endTime = new Date();
+    // Get the duration in Miliseconds
+    const duration = endTime - startTime;
     // Log Details; TImestamp, method, URL, and response status
-    const logDetails = `[${startTime.toISOString()}] ${req.method} ${req.url} ${res.statusCode}\n`;
+    const logDetails = `[Start Time: ${startTime}] ${req.method} ${req.url} ${
+      res.statusCode
+    } [End Time: ${endTime}] [Duration: ${duration}ms]\n`;
     // Append the log details to 'request_logs.txt' file
-    fs.appendFile(path.join(__dirname, 'Logs', 'request_logs.txt'), logDetails, (err)=>{
-        if(err){
-            console.log("Failed to write logs:", err)
+    fs.appendFile(
+      path.join(__dirname, "Logs", "request_logs.txt"),
+      logDetails,
+      (err) => {
+        if (err) {
+          console.log("Failed to write logs:", err);
         }
-    })
-
-})
+      }
+    );
+  });
+  // Move to the next Middleware
+  next();
+});
 
 // Middle to parse JSON bodies
 app.use(express.json());
@@ -34,12 +44,12 @@ app.use("/authors", authorRouter);
 app.use("/quotes", quoteRouter);
 
 // Set request handlers based on the API endpoints
-app.get("/", (req, res)=>{
-    res.send(`<h1 style="color: blue">Welcome to my quote API</h1>`)
+app.get("/", (req, res) => {
+  res.send(`<h1 style="color: blue">Welcome to my quote API</h1>`);
 });
 
-// Set the server to listen on port 5000
+// Set the server to listen on port 3000
 const PORT = 3000;
-app.listen(PORT, ()=>{
-    console.log(`Server is listening on http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
 });
